a1b558bd38606a7dc71d448fe1ab034e4ec9763f
smaury01 <smaury01@cst-tp-b10114>
2021-01-18 10:45:09 +0100

    TableauController

---
+++ src/main/java/galerie/controller/TableauController.java
@@ -0,0 +1,91 @@
+package galerie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import galerie.dao.GalerieRepository;
+import galerie.entity.Galerie;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ * Edition des catégories, sans gestion des erreurs
+ */
+@Controller
+@RequestMapping(path = "/tableau")
+public class TableauController {
+
+    @Autowired
+    private TableauRepository dao;
+
+    /**
+     * Affiche toutes les catégories dans la base
+     *
+     * @param model pour transmettre les informations à la vue
+     * @return le nom de la vue à afficher ('afficheGaleries.html')
+     */
+    @GetMapping//(path = "show")
+    public String afficheTousLesTableaux(Model model) {
+        model.addAttribute("tableaux", dao.findAll());
+        return "afficheTableau";
+    }
+
+    /**
+     * Montre le formulaire permettant d'ajouter une galerie
+     *
+     * @param galerie initialisé par Spring, valeurs par défaut à afficher dans le formulaire
+     * @return le nom de la vue à afficher ('formulaireGalerie.html')
+     */
+    @GetMapping(path = "add")
+    public String montreLeFormulairePourAjout(@ModelAttribute("tableau") Tableau tableau) {
+        return "formulaireTableau";
+    }
+
+    /**
+     * Appelé par 'formulaireGalerie.html', méthode POST
+     *
+     * @param galerie Une galerie initialisée avec les valeurs saisies dans le formulaire
+     * @param redirectInfo pour transmettre des paramètres lors de la redirection
+     * @return une redirection vers l'affichage de la liste des galeries
+     */
+    @PostMapping(path = "save")
+    public String ajouteLeTableauPuisMontreLaTable(Tableau tableau, RedirectAttributes redirectInfo) {
+        String message;
+        try {
+            // cf. https://www.baeldung.com/spring-data-crud-repository-save
+            dao.save(tableau);
+            // Le code de la catégorie a été initialisé par la BD au moment de l'insertion
+            message = "Le tableau '" + tableau.getTitre() + "' a été correctement enregistrée";
+        } catch (DataIntegrityViolationException e) {
+            // Les noms sont définis comme 'UNIQUE' 
+            // En cas de doublon, JPA lève une exception de violation de contrainte d'intégrité
+            message = "Erreur : Le tableau '" + tableau.getTitre() + "' existe déjà";
+        }
+        // RedirectAttributes permet de transmettre des informations lors d'une redirection,
+        // Ici on transmet un message de succès ou d'erreur
+        // Ce message est accessible et affiché dans la vue 'afficheGalerie.html'
+        redirectInfo.addFlashAttribute("message", message);
+        return "redirect:show"; // POST-Redirect-GET : on se redirige vers l'affichage de la liste		
+    }
+
+    /**
+     * Appelé par le lien 'Supprimer' dans 'afficheGaleries.html'
+     *
+     * @param galerie à partir de l'id de la galerie transmis en paramètre, Spring fera une requête SQL SELECT pour
+     * chercher la galerie dans la base
+     * @param redirectInfo pour transmettre des paramètres lors de la redirection
+     * @return une redirection vers l'affichage de la liste des galeries
+     */
+    @GetMapping(path = "delete")
+    public String supprimeUnTableauPuisMontreLaTable(@RequestParam("id") Galerie galerie, RedirectAttributes redirectInfo) {
+        String message = "Le tableau '" + tableau.getTitre() + "' a bien été supprimée";
+            dao.delete(tableau);       
+        return "redirect:show"; 
+    }
+}
